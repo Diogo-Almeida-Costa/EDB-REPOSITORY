6c3b0a3ed5bef2e5211a4651d50cc7631bed706f
Diogo-Almeida-Costa <jdiogoalmeida988@gmail.com>
2024-07-22 21:10:32 -0300

    Implementação do TAD coonjunto por meio de vetor ok

--- Unidade 2/Conjunto - Array/TAD_Conjunto-Array/src/Conjunto.cpp
+++ Unidade 2/Conjunto - Array/TAD_Conjunto-Array/src/Conjunto.cpp
@@ -1,38 +1,78 @@
 #include "Conjunto.h"
 #include <iostream>
 
-Conjunto::Conjunto(int capacidade)
+Conjunto::Conjunto(int _capacidade)
 {
-    //TO-DO
+    this->capacidade = _capacidade;
+    this->elementos = new int[this->capacidade];
+    this->quantidade = 0;
 }
 
 Conjunto::~Conjunto()
 {
-    //TO-DO
+    delete[] elementos;
 }
 
+//repare que é uma busca sequencial, portanto é de complexidade linear também
+//Se mantivermos ordenação podemos usar busca binária e ter custo log(n)
 bool Conjunto::buscar(int elemento) const
 {
-   //TO-DO
-    return elemento != 0;
+   for(int i = 0; i < this->quantidade; i++)
+   {
+        if(this->elementos[i] == elemento)
+        {
+            return true;
+        }
+   }
+   return false;
 }
 
+//Pior caso é linear, precisa percorrer todo mundo
+//Se mantivermos ordenação vamos ter um custo maior, visto que uma vez que inserirmos um elemento iremos
+//ter que mover todos os elementos maiores que ele.
 bool Conjunto::inserir(int elemento)
 {
-    //TO-DO
-    return elemento != 0;
+    if(this->quantidade == this->capacidade)
+    {
+        return false;
+    }
+    for(int i = 0; i < quantidade; i++)
+    {
+        if(this->elementos[i] == elemento)
+        {
+            return false;
+        }
+    }
+    this->elementos[this->quantidade] = elemento;
+
+    ++this->quantidade;
+    return true;
 }
 
+//Os piores casos são: tentar remover um elemento que não esta ou tentar remover o 
+//primeiro(vai ter que deslocar quase todo os elementos), que é de complexidade linear também
+//Mesmo se deixarmos o array ordenado vamos ter o caso linear quando formos remover o primeiro elemento
 bool Conjunto::remover(int elemento)
 {
-    //TO-DO
-    return elemento != 0;
+    for(int i = 0; i < this->quantidade; i++)
+    {
+        if(this->elementos[i] == elemento)
+        {
+            for(int j = i; j < this->quantidade-1; j++)
+            {
+                this->elementos[j] = this->elementos[j+1];
+            }
+            --this->quantidade;
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int Conjunto::tamanho() const
 {
-    //TO-DO
-    return 0;
+    return quantidade;
 }
 
 void Conjunto::imprimir() const
